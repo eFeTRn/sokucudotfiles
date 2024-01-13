bd2893a26d930fb8e1d472fdfec815fda9a2929e
eFeTRn <109210874+eFeTRn@users.noreply.github.com>
2024-01-13 23:30:33 +0300

    Add files via upload

---
+++ remove.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "dunst, fish, hypr, kitty, rofi and waybar"
+read -rsp $'are you sure? this script will remove the folders thats stated above and reinstall them (reinstalling only works for arch distros)\n'
+cd ~/.config/
+rm -rf dunst/ fish/ hypr/ kitty/ rofi/ waybar/
+echo "finished removing: dunst, fish, hypr, kitty, rofi and waybar folders"
+echo "---installing packages---"
+if [ $(lsb_release -is)  == "Arch" ]
+then
+sudo pacman -S dunst fish hyprland kitty rofi waybar
+else
+echo "installing packages is only supported for arch"
+fi
