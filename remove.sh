fc56450c1046086cc9d558d5236440b62219a160
eFeTRn <109210874+eFeTRn@users.noreply.github.com>
2024-01-13 23:55:01 +0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -3,6 +3,8 @@ echo "dunst, fish, hypr, kitty, rofi and waybar"
 read -rsp $'are you sure? this script will remove the folders thats stated above and reinstall them (reinstalling only works for arch)\n'
 cd ~/.config/
 rm -rf dunst/ fish/ hypr/ kitty/ rofi/ waybar/
+cd ~
+rm wall.png
 echo "finished removing: dunst, fish, hypr, kitty, rofi and waybar folders"
 echo "---installing packages---"
 if [ $(lsb_release -is)  == "Arch" ]
