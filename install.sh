a75ebe1347793412f5b601a22a9a6c78a53725c2
eFeTRn <109210874+eFeTRn@users.noreply.github.com>
2024-01-13 23:01:00 +0300

    auto install using shell

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+tar xzf hyprconfs.tar.gz
+                            #cannot figure out how to supply multiple folders, it just says too many arguments, fuck
+                            # so i needed to come up with a better idea
+                            # but i'm still keeping this maybe someday i'll figure this out
+                            # -------------------------------------------------------------
+                            #if [ -e dunst/ fish/ hypr/ kitty/ rofi/ waybar/ ]; then
+                            #rm -rf dunst/ fish/ hypr/ kitty/ rofi/ waybar/
+                            #mkdir dunst/ fish/ hypr/ kitty/ rofi/ waybar/
+                            #fi
+                            # -------------------------------------------------------------
+yes | cp -r dunst/ fish/ hypr/ kitty/ rofi/ waybar/ ~/.config/
+rm -rf dunst/ fish/ hypr/ kitty/ rofi/ waybar/
+echo "finished"
