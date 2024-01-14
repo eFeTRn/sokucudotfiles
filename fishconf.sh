4dad51c3810fdd5d47442420a3bc46f8d1c4b0d1
eFeTRn <109210874+eFeTRn@users.noreply.github.com>
2024-01-14 14:26:18 +0300

    Update fishconf.sh

--- fishconf.sh
+++ fishconf.sh
@@ -1,4 +1,6 @@
 #!/bin/fish
-yes | fish_config theme choose Nord
+# yes | fish_config theme choose Nord
+yes | fish_config theme choose "ayu Mirage"
 yes | fish_config prompt choose scales
 yes | fish_config prompt save
+set -U fish_greeting ""
