aca7cac735e874a65b98c4af6441259ebe10dc88
eFeTRn <109210874+eFeTRn@users.noreply.github.com>
2024-01-16 18:18:19 +0300

    Update p-ins.sh

--- p-ins.sh
+++ p-ins.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 read -rsp $'this will install needed packages\n'
-sudo pacman -S dunst
-sudo pacman -S fish
-sudo pacman -S kitty
-sudo pacman -S rofi
-sudo pacman -S waybar
-echo "packages installed"
+sudo pacman -S --needed dunst fish kitty rofi waybar
+echo "script finished"
